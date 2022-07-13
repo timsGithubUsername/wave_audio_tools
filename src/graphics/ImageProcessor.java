e42899edaeb91aef4744388e10d6a0a3a93ff009
DESKTOP-T0T65RF\timro <tim.romonath@gmail.com>
2022-07-13 18:42:38 +0200

    refactored image processor with weight

--- src/graphics/ImageProcessor.java
+++ src/graphics/ImageProcessor.java
@@ -4,29 +4,32 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class ImageProcessor {
+    private static final int OFFSET = 20;
+
     public static BufferedImage processWithWeight(int xSize, int ySize, float[] data) {
-        int offset = 20;
+        //calculate which points from the data array are plotted
         int step = data.length / xSize;
-
-        float scaleFactor = (ySize - offset) / (2 * getMax(data));
-
+        //set the scale factor so that the highest value isnt bigger than ySize
+        float scaleFactor = (ySize - OFFSET) / (2 * getMax(data));
+        //prepare image
         BufferedImage image = new BufferedImage(xSize, ySize, BufferedImage.TYPE_4BYTE_ABGR);
         Graphics2D graphics = image.createGraphics();
         graphics.setPaint (new Color(255, 255, 255));
         graphics.fillRect ( 0, 0, xSize, ySize);
 
-        float temp;
-        int tempVal;
+        float temp; //holds the hight of the current peak as float
+        int tempVal; //the hight on the plot
         int black = new Color(0, 0, 0).getRGB();
-
-        for (Integer x = 0; x < xSize; x++) {
+        //for every point at length
+        for (int x = 0; x < xSize; x++) {
             temp = 0;
+            //calculate average over step
             for(int i = x; i < x + step; i++) temp += scaleFactor * data[step * x];
             tempVal = (int) (temp / step);
-
+            //if positive fill upper halve...
             if(tempVal >= 0) {
                 for(int y = ySize - (tempVal + ySize/2); y <= ySize/2; y++) image.setRGB(x, y, black);
-            }
+            } //...else fill under halve
             else {
                 for(int y = ySize/2; y < Math.abs(tempVal) + ySize/2; y++) image.setRGB(x, y, black);
             }
@@ -35,10 +38,9 @@ public class ImageProcessor {
         return image;
     }
     public static BufferedImage process(int xSize, int ySize, float[] data) {
-        int offset = 20;
         int step = data.length / xSize;
 
-        float scaleFactor = (ySize - offset) / getMax(data);
+        float scaleFactor = (ySize - OFFSET) / getMax(data);
 
         BufferedImage image = new BufferedImage(xSize, ySize, BufferedImage.TYPE_4BYTE_ABGR);
 
